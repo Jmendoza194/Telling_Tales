9538ea74ac91084a01e1ebd92c10de26eaee46b9
Jose Mendoza <joseemendoza194@gmail.com>
2020-03-11 11:43:58 -0700

    removed background info

--- src/components/CardPop.js
+++ src/components/CardPop.js
@@ -9,14 +9,7 @@ const CardPop = (props) =>{
                 <div className='popContainer'>
                     <h1>{props.hero}</h1> 
                     <br></br>
-                        <div>
-                            <h3>Background Info</h3> 
-                            <p>{props.description.Background_info}</p>
-                            <br></br>
-                        </div>
-                        <div>
-                            <h3>Meme:</h3> 
-                        </div>
+                        
                         <div className='Image_Container'>  
                             <img className='Meme' src={props.description.meme} alt='Achilles'/>
                         </div>
